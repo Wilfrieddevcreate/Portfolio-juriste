57e0ae4500a0d8452d64dac285474d97be1739e3
Wilfrieddevcreate <yves.heloussato@epitech.eu>
2024-09-03 12:55:43 +0100

    Ajout des service

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -1,44 +1,81 @@
 import React from "react";
-import AboutPhoto from "../assets/about.jpg";
-import { Link } from "react-router-dom";
+import useSWR from "swr";
+//import { Link } from "react-router-dom";
+import AproposService from "../service/about.service";
+
+const fetchApropos = async () => {
+  const data = await AproposService.fetchApropos();
+  return data[0]; // assuming you want the first item
+};
+
+// Fonction pour limiter le contenu HTML à un certain nombre de mots
+const limitHtmlContent = (htmlContent: string, wordLimit: number) => {
+  const div = document.createElement("div");
+  div.innerHTML = htmlContent;
+
+  let wordCount = 0;
+  const walker = document.createTreeWalker(div, NodeFilter.SHOW_TEXT, null);
+
+  while (walker.nextNode() && wordCount < wordLimit) {
+    const node = walker.currentNode as Text;
+    const words = node.textContent?.split(/\s+/) || [];
+
+    if (wordCount + words.length > wordLimit) {
+      const excessWords = wordCount + words.length - wordLimit;
+      node.textContent = words.slice(0, -excessWords).join(" ") + "...";
+    }
+
+    wordCount += words.length;
+  }
+
+  return div.innerHTML;
+};
+
 const About: React.FC = () => {
-  return (
-    <div className="">
-      <div className="container mx-auto py-12 px-4 ">
-        <div className="text-center mb-12">
-          <h2 className="text-4xl md:text-5xl font-bold text-gray-800 fade-in">
-            À propos
-          </h2>
-        </div>
-        <div className="flex flex-col md:flex-row items-center justify-center space-y-8 md:space-y-0 md:space-x-12">
-          {/* Image à gauche */}
-          <div className="w-full md:w-1/2 fade-in">
-            <img
-              src={AboutPhoto}
-              alt="About Image"
-              className="rounded-lg shadow-lg w-auto"
-            />
-          </div>
-          {/* Texte à droite */}
-          <div className="w-full md:w-1/2 text-center md:text-left fade-in">
-            <h3 className="text-3xl font-semibold text-gray-800 mb-4">
-              Découvrez Notre Mission
-            </h3>
-            <p className="text-gray-600 leading-relaxed mb-6">
-              Nous nous engageons à offrir des solutions innovantes pour
-              améliorer la vie quotidienne des gens. Nos services sont conçus
-              pour être accessibles à tous et pour répondre aux besoins
-              modernes. Découvrez nos projets actuels, nos objectifs et la
-              manière dont nous apportons des changements positifs.
-            </p>
-            <Link to={"/about"}>
-              <button className="bg-blue-600 text-white px-6 py-3 rounded-full shadow-md hover:bg-blue-700 transition duration-300">
-                Voir plus
-              </button>
-            </Link>
-          </div>
-        </div>
+  const { data: aboutData, error } = useSWR('/v1/abouts', fetchApropos);
+
+  if (error) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <p>Erreur lors du chargement des données.</p>
       </div>
+    );
+  }
+
+  if (!aboutData) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <p>Chargement...</p>
+      </div>
+    );
+  }
+
+  const limitedContent = limitHtmlContent(aboutData.content, 40);
+
+  return (
+    <div className="container mx-auto py-12 px-4">
+      <div className="text-center mb-12">
+        <h2 className="text-4xl md:text-5xl font-bold text-gray-800 fade-in">
+          À propos
+        </h2>
+      </div>
+      <div className="flex flex-col md:flex-row items-center justify-center space-y-8 md:space-y-0 md:space-x-12">
+        {/* Image à gauche */}
+        <div className="w-full md:w-1/2 fade-in">
+          <img
+            src={aboutData.image_url}
+            alt="About Image"
+            className="rounded-lg shadow-lg w-auto"
+          />
+        </div>
+        {/* Texte à droite */}
+        <div
+          className="w-full md:w-1/2 text-center md:text-left fade-in"
+          dangerouslySetInnerHTML={{ __html: limitedContent }}
+        />
+        
+      </div>
+     
     </div>
   );
 };
---
+++ src/service/about.service.ts
@@ -0,0 +1,17 @@
+import api from './base.service'
+
+class AproposService {
+  // A propos
+  // Récupérer toutes les données
+  async fetchApropos() {
+    try {
+      const response = await api.get(`/abouts`);
+      return response.data;
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+ }
+
+export default new AproposService()
