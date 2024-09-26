92d12b304d5199678e09194a4bd7b51632e5d4bc
Wilfrieddevcreate <yves.heloussato@epitech.eu>
2024-09-27 00:23:23 +0100

    Correction

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -19,7 +19,6 @@ export default function Home() {
         <p  className="text-center">Accédez aux publications  juridiques essentiels publiés par nos experts. Cliquez ci-dessous pour en savoir plus.
         </p>
          <br />
-        
         <div className="flex justify-center">
           <Link to={"/publications"}>
             <button className="bg-white text-blue-800 p-2 rounded-full">Lire les publications
